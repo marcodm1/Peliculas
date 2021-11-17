511c1a13d8b4fe300aaca24f99f76925fd55fea4
marcodm1 <marcodomat@gmail.com>
2021-11-17 11:24:19 +0100

    pruebas con back

--- src/components/menuLateral/sponsors/Sponsors.jsx
+++ src/components/menuLateral/sponsors/Sponsors.jsx
@@ -1,10 +1,38 @@
-// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {useState, useEffect} from 'react';
 import './Sponsors.css';
 
 const Sponsors = () => {
+    const [error, setError] = useState(null);
+    const [sponsors, setSponsors] = useState([]);
+  
+    useEffect(() => {        
+        fetch("http://marcodmapi.atwebpages.com/")
+            .then(resultado => resultado.json())
+            .then(
+                data => {setSponsors(data)}, 
+                error => setError(error) 
+            ) 
+    }, []);
 
+    // fetch('http://localhost:300/users')
+    //   .then(response => {
+    //     //do something with response
+    //     const users = response.json();
+    //     this.setState({ users })
+    //   })
+    //   .catch(err => {
+    //     throw new Error(err)
+    //   })
+      
     return (
-        <div className="sponsors">Sponsors</div>
-    )
+        <>
+        <div className="sponsors">Sponsords</div>
+        {console.log(sponsors)}
+        {/* <ul>
+            <p>{sponsors.map(sponsor => <li>{sponsors} </li>)}</p>
+        </ul> */}
+        </>
+    );
+    
 }
 export default Sponsors;
