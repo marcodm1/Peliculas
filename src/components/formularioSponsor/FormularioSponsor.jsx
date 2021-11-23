1a26d89975fdd501b511f9b9331482257fd4eaa7
marcodm1 <marcodomat@gmail.com>
2021-11-23 11:35:49 +0100

    reajusto el responsive

--- src/components/formularioSponsor/FormularioSponsor.jsx
+++ src/components/formularioSponsor/FormularioSponsor.jsx
@@ -29,7 +29,7 @@ const FormularioSponsor = () => {
         {/* <p>nombre = {nombre}</p> */}
         <form onSubmit={handleSubmit(onsubmit)}>
             {/* nombre de la empresa */}
-            <input type="text" className="a" placeholder='Nombre de la empresa' name="nombre" 
+            <input type="text" className="area" placeholder='Nombre:' name="nombre" 
                 {...register("nombre", { 
                     required: { value: true, message: 'Introduzca el Nombre de la empresa.' },
                     minLength: { value: 3, message: 'Introduzca mínimo 3 caractéres.' },
@@ -39,7 +39,7 @@ const FormularioSponsor = () => {
             <div className="mensajeError"> { errors.nombre &&  errors.nombre.message } </div>
             
             {/* direccion */}
-            <input  type="text" className="a" placeholder='Dirección de la empresa' name="direccion" 
+            <input  type="text" className="area" placeholder='Dirección:' name="direccion" 
                 {...register("direccion", {      
                     required: { value: true, message: 'Introduzca la dirección de la empresa.' },
                     minLength: { value: 3, message: 'Introduzca mínimo 3 caractéres.' },
@@ -49,7 +49,7 @@ const FormularioSponsor = () => {
             <div className="mensajeError"> { errors.direccion &&  errors.direccion.message } </div>
             
             {/* dias que va a contratar */}
-            <input type="number" className="a" placeholder='Días que va a contratar' name="dias" 
+            <input type="number" className="area" placeholder='Días:' name="dias" 
                 {...register("dias", { 
                     required: { value: true, message: 'Introduzca número de dias a contratar.' }})
                 }   
@@ -58,7 +58,7 @@ const FormularioSponsor = () => {
 
             {/* código de la empresa */}
 
-            <input type="number" className="a" placeholder='Código de la empresa' name="codigo" 
+            <input type="number" className="area" placeholder='Código:' name="codigo" 
                 {...register("codigo", {      
                     required: { value: true, message: 'Introduzca el código de la empresa.' },
                     minLength: { value: 15, message: 'Longitud del código tiene que contenet 15 números.' },
@@ -67,7 +67,6 @@ const FormularioSponsor = () => {
             />
             <div className="mensajeError">{ errors.codigo &&  errors.codigo.message }</div>
 
-            <div className="precio"> El precio total sería de: 0€/dia </div>
             <ul>
                 { entradas.map(envio => 
                     <li key={envio.nombre}> {envio.nombre} - {envio.direccion} - {envio.dias} - {envio.codigo}</li>
