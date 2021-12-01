3a5f97e0d714993f86c037f0af0f2914f07ec851
marcodm1 <marcodomat@gmail.com>
2021-12-01 09:43:11 +0100

    ahora si esta ok la app

--- src/components/zonaSponsors/ZonaSponsors.jsx
+++ src/components/zonaSponsors/ZonaSponsors.jsx
@@ -1,19 +1,19 @@
-import {useState, useEffect} from 'react';
-import {getSponsors} from '../../funciones/httpClient';
+import { useState, useEffect } from 'react';
+import { getSponsors } from '../../funciones/httpClient';
 import './ZonaSponsors.css';
 
 const Sponsors = () => {
     const [sponsors, setSponsors] = useState([]);
 
-    useEffect(() => {        
+    useEffect(() => {
         getSponsors().then(data => setSponsors(data))
     }, []);
 
     return (
         <>
-        {sponsors.map(sponsor => <div className="zonaSponsor">{sponsor.nombreEmpresa}</div>)}
+            {sponsors.map(sponsor => <div className="zonaSponsor">{sponsor.nombreEmpresa}</div>)}
         </>
     );
-    
+
 }
 export default Sponsors;
